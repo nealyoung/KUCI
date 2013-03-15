3abb738fb1f83ea24e360979d36b172f9a820abf
Neal Young <hi@nealyoung.me>
2013-03-14 22:58:48 -0700

    Initial commit

---
+++ NavTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NavTest
+//
+//  Created by Nealon Young on 12/15/12.
+//  Copyright (c) 2012 Nealon Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
